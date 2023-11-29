afa24329e9cc072f15ab810fb22704b7717609dd
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-11-29 10:50:47 +0100

    Gjort klar del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //a
+        int sum = addera(14, 34);
+        System.out.println("14 + 34 = "+sum);
+        sum = addera(1212, 333);
+        System.out.println("1212 + 333 = "+sum);
+    }
+    static int addera(int tal1, int tal2)
+    {
+        int summa = tal1+tal2;
+        return summa;
     }
 }
