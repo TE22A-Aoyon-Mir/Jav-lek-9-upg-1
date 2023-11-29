4fcdf367fe7a1279fab6772f3632f1669a5c570c
Aoyon Mir <aoyon.mir@elev.ga.ntig.se>
2023-11-29 11:00:17 +0100

    Gjort klar del b

--- src/App.java
+++ src/App.java
@@ -6,10 +6,25 @@ public class App {
         System.out.println("14 + 34 = "+sum);
         sum = addera(1212, 333);
         System.out.println("1212 + 333 = "+sum);
+
+        //b
+        int sum2 = minus(14, 34);
+        System.out.println("14 - 34 = "+sum2);
+        sum2 = minus(1212, 333);
+        System.out.println("1212 - 333 = "+sum2);
     }
+
+    //a
     static int addera(int tal1, int tal2)
     {
         int summa = tal1+tal2;
+        return summa;
+    }
+
+    //b
+    static int minus(int tal1, int tal2)
+    {
+        int summa = tal1-tal2;
         return summa;
     }
 }
